2431ea47514ea01d1d13b3cdfc52f936041765dd
vitaliyprus <dev.prus@gmail.com>
2018-01-23 19:54:20 +0300

    Add api response logging

--- app/src/main/java/vision/genesis/clientapp/di/modules/ApiModule.java
+++ app/src/main/java/vision/genesis/clientapp/di/modules/ApiModule.java
@@ -7,10 +7,12 @@ import dagger.Provides;
 import io.swagger.client.ApiClient;
 import io.swagger.client.JSON;
 import io.swagger.client.api.AccountApi;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 import vision.genesis.clientapp.BuildConfig;
+import vision.genesis.clientapp.net.LogJsonInterceptor;
 
 /**
  * GenesisVision
@@ -22,20 +24,29 @@ public class ApiModule
 {
 	@Provides
 	@Singleton
-	Retrofit.Builder provideAdapterBuilder() {
-		return new Retrofit
-				.Builder()
-				.baseUrl(BuildConfig.API_ADDRESS)
-				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-				.addConverterFactory(ScalarsConverterFactory.create())
-				.addConverterFactory(vision.genesis.clientapp.net.GsonCustomConverterFactory.create(new JSON().getGson()));
+	OkHttpClient provideHttpClient() {
+		OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder()
+				.addInterceptor(new LogJsonInterceptor());
+		return httpClientBuilder.build();
 	}
 
 	@Provides
 	@Singleton
-	ApiClient provideApiClient(Retrofit.Builder adapterBuilder) {
+	Retrofit.Builder provideAdapterBuilder() {
+		return new Retrofit.Builder()
+				.addConverterFactory(ScalarsConverterFactory.create())
+				.addConverterFactory(vision.genesis.clientapp.net.GsonCustomConverterFactory.create(new JSON().getGson()))
+				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+				.baseUrl(BuildConfig.API_ADDRESS);
+
+	}
+
+	@Provides
+	@Singleton
+	ApiClient provideApiClient(Retrofit.Builder adapterBuilder, OkHttpClient httpClient) {
 		ApiClient apiClient = new ApiClient();
 		apiClient.setAdapterBuilder(adapterBuilder);
+		apiClient.configureFromOkclient(httpClient);
 		return apiClient;
 	}
 
---
+++ app/src/main/java/vision/genesis/clientapp/net/LogJsonInterceptor.java
@@ -0,0 +1,33 @@
+package vision.genesis.clientapp.net;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import timber.log.Timber;
+import vision.genesis.clientapp.BuildConfig;
+
+/**
+ * GenesisVision
+ * Created by Vitaly on 1/23/18.
+ */
+
+public class LogJsonInterceptor implements Interceptor
+{
+	@Override
+	public Response intercept(Chain chain) throws IOException {
+		Request request = chain.request();
+		Response response = chain.proceed(request);
+		String rawJson = response.body().string();
+
+		if (!BuildConfig.PROD) {
+			Timber.tag("REST");
+			Timber.d("REQUEST %s\nRESPONSE %s", request.url(), rawJson);
+		}
+		// Re-create the response before returning it because body can be read only once
+		return response.newBuilder()
+				.body(ResponseBody.create(response.body().contentType(), rawJson)).build();
+	}
+}
