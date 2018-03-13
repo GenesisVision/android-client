f600d7502c8c6b361e187b8f237f56862948e5ed
vitaliyprus <dev.prus@gmail.com>
2018-03-13 12:41:21 +0300

    Add Bar Chart to Profit Chart

--- app/src/main/java/vision/genesis/clientapp/ui/ProfitChartView.java
+++ app/src/main/java/vision/genesis/clientapp/ui/ProfitChartView.java
@@ -5,8 +5,12 @@ import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -28,12 +32,12 @@ import vision.genesis.clientapp.R;
 
 public class ProfitChartView extends RelativeLayout
 {
-	@BindView(R.id.line_chart)
-	public LineChart chart;
+	@BindView(R.id.combined_chart)
+	public CombinedChart chart;
 
 	private int fillColor = R.color.grey300;
 
-	private int lineColor = R.color.grey400;
+	private int lineColor = R.color.colorPrimaryDark;
 
 	public ProfitChartView(Context context) {
 		super(context);
@@ -89,41 +93,49 @@ public class ProfitChartView extends RelativeLayout
 	}
 
 	public void setDataDouble(List<Double> data) {
-		List<Entry> entries = new ArrayList<>();
-		float index = 0;
-		for (Double value : data) {
-			entries.add(new Entry(index, value.floatValue()));
-			index++;
-		}
-		setData(entries);
+//		List<Entry> entries = new ArrayList<>();
+//		float index = 0;
+//		for (Double value : data) {
+//			entries.add(new Entry(index, value.floatValue()));
+//			index++;
+//		}
+//		setLineData(entries);
 	}
 
 	public void setChart(List<Chart> charts) {
-		List<Entry> entries = new ArrayList<>();
-		float index = 0;
-		for (Chart chart : charts) {
-			entries.add(new Entry(index, chart.getTotalProfit().floatValue()));
-			index++;
-		}
-		setData(entries);
-	}
-
-	public void setData(List<Entry> data) {
-		if (data.size() == 0) {
+		if (charts.isEmpty()) {
 			chart.clear();
 			return;
 		}
+		List<Entry> lineEntries = new ArrayList<>();
+		List<BarEntry> barEntries = new ArrayList<>();
+		float index = 0;
+		for (Chart chart : charts) {
+			lineEntries.add(new Entry(index, chart.getTotalProfit().floatValue()));
+			barEntries.add(new BarEntry(index, new float[]{
+					chart.getFund().floatValue(),
+					chart.getLoss().floatValue(),
+					chart.getProfit().floatValue()}));
+			index++;
+		}
+
+		CombinedData data = new CombinedData();
+		data.setData(getLineData(lineEntries));
+		data.setData(getBarData(barEntries));
+
+		chart.setData(data);
+	}
+
+	private LineData getLineData(List<Entry> data) {
 		Collections.sort(data, new EntryXComparator());
 		LineData lineData = new LineData();
 
-		lineData.addDataSet(createDataSet(data));
+		lineData.addDataSet(createLineDataSet(data));
 
-		chart.setData(lineData);
-		chart.invalidate();
-//		chart.animateX(1200, input -> (1 - (1 - input) * (1 - input)));
+		return lineData;
 	}
 
-	private LineDataSet createDataSet(List<Entry> data) {
+	private LineDataSet createLineDataSet(List<Entry> data) {
 		LineDataSet dataSet = new LineDataSet(data, "");
 
 		dataSet.setLabel("");
@@ -131,10 +143,30 @@ public class ProfitChartView extends RelativeLayout
 		dataSet.setDrawCircles(false);
 		dataSet.setColor(ContextCompat.getColor(getContext(), lineColor));
 		dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-		dataSet.setLineWidth(1f);
+		dataSet.setLineWidth(2f);
 
-		dataSet.setFillColor(ContextCompat.getColor(getContext(), fillColor));
-		dataSet.setDrawFilled(true);
+		return dataSet;
+	}
+
+	private BarData getBarData(List<BarEntry> data) {
+		Collections.sort(data, new EntryXComparator());
+		BarData barData = new BarData();
+
+		barData.addDataSet(createBarDataSet(data));
+
+		return barData;
+	}
+
+	private BarDataSet createBarDataSet(List<BarEntry> data) {
+		BarDataSet dataSet = new BarDataSet(data, "");
+
+		dataSet.setLabel("");
+		dataSet.setDrawValues(false);
+		dataSet.setColor(ContextCompat.getColor(getContext(), lineColor));
+
+		dataSet.setColors(ContextCompat.getColor(getContext(), R.color.colorPrimary),
+				ContextCompat.getColor(getContext(), R.color.transactionRed),
+				ContextCompat.getColor(getContext(), R.color.transactionGreen));
 
 		return dataSet;
 	}
